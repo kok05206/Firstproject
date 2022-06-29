0048ad3caca44a84089939270cdd29190a6d8188
kok05206 <ghkddlsdhk37@naver.com>
2022-06-29 18:00:51 +0900

    My firstproject_Chapter10 - Learn 'lombok' - Learn 'refactoring'

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // 로깅을 위한 어노테이션!!
 public class ArticleController {
 
     @Autowired // 스프링 부트가 미리 생성해놓은 객체를 가져다가 자동으로 연결!
@@ -21,15 +23,19 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        // 로깅이란 ? --> 서버에서 일어나는 일들을 다 기록할 수 있게 하는것.
+        // System.out.println(form.toString()); --> 로깅 기능으로 대체!!
+        log.info(form.toString()); // 이렇게!!
 
         // 1. DTO를 Entity로 변환!!
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        // System.out.println(article.toString());
+        log.info(article.toString()); // print문을 다 변경이 가능!!
 
         // 2. Repository 에게 Entity를 DB안에 저장하게 함!
        Article saved  = articleRepository.save(article);
-        System.out.println(saved.toString());
+        // System.out.println(saved.toString());
+        log.info(saved.toString()); // 마찬가지!!
 
         return "";
     }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,17 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+// 생성자 파트 리팩터링 과정
+@AllArgsConstructor // 매번 생성자를 써주기 귀찮아서 간소화 해주기 위한 어노테이션
+@ToString // ToString메소드를 간소화 해주기 위한 어노테이션
 public class ArticleForm {
 
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
     
     // 변환 메소드 클래스 선언
     public Article toEntity() {
