5a402d25b60025d519fb3e4d7df7a3b00dedf268
kok05206 <ghkddlsdhk37@naver.com>
2022-07-04 17:26:08 +0900

    My firstproject_Chapter16 데이터삭제의 흐름 3단계 1. 삭제요청이 전달! -> HTTP를 통해 전달이 된다! 2. DB에서 찾고 삭제! -> HTTP를 통해 삭제요청을 전달받은 컨트롤러가 repositoty를 통해서 DB속 데이터를 삭제! * 단 기존 데이터가 있는경우에만 삭제해야한다! * 3. 결과 페이지로 redirect! -> 삭제가 완료가 되었다면 결과페이지로 redirect! 이때 삭제완료 메세지를 띄우기 위한 클래스 : RedirectAttributes!
    해당객체 속의 메소드
    .addFlashAttributes : redirect된 페이지에서 휘발성으로 사용할 데이터를 등록하게 한다!

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,10 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -115,5 +113,23 @@ public class ArticleController {
 
         // 3. 수정 결과 페이지로 Redirect 한다!
         return "redirect:/articles/" + articleEntity.getId();
+    }
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청이 들어왔습니다!");
+
+        // 삭제 처리 개요 3가지.
+        // 1. 삭제 대상을 가져온다!
+        Article target  = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 2. 대상을 삭제 한다!
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제가 완료되었습니다!");
+        }
+
+        // 3. 결과 페이지로 redirect 한다!
+        return "redirect:/articles";
     }
 }
