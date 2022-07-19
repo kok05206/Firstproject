747a5e337a21ff20237d6cb0e403bd4166b76d8e
kok05206 <ghkddlsdhk37@naver.com>
2022-07-19 16:18:45 +0900

    My firstproject_Chapter31 Jackson? ObjectMapper? ObjectNode? -------------------------------------- 강의 목표 ObjectMapper를 활용하여, JSON과 자바 객체간 변환을 연습하시오. -------------------------------------- ObjectMapper : 제이슨과 자바객체간 변환을 제공하는 객체 앞서 요청한 JSON을 DTO로 만들고  응답을 위한 DTO를 JSON으로 변환시킨 객체
    실습 개요
    JSON을 자바객체로 변환해보고, 반대로 자바객체를 JSON으로 변환해본다!
    --------------------------------------

--- src/main/java/com/example/firstproject/aop/DebuggingAspect.java
+++ src/main/java/com/example/firstproject/aop/DebuggingAspect.java
@@ -14,7 +14,10 @@ import org.springframework.stereotype.Component;
 public class DebuggingAspect {
 
     // 대상 메소드 선택 : CommentService#create()
-    @Pointcut("execution(* com.example.firstproject.service.CommentService.*(..))") // 주입 대상 지정
+    //@Pointcut("execution(* com.example.firstproject.service.CommentService.*(..))") // 주입 대상 지정
+
+    //대상 메소드 선택 : api 패키지의 모든 메소드
+    @Pointcut("execution(* com.example.firstproject.api.*.*(..))")
     private void cut() {}
 
     // 실행 시점 설정 : cut()의 대상이 수행되기 이전에 수행
